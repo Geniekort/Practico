73e00ac9ad74d1f9a790020f02373731a0938ee2
GerritKaasjager <gerritkaasjager@solcon.nl>
2016-01-01 17:20:37 +0100

    Tester 2

--- Practico/src/View.java
+++ Practico/src/View.java
@@ -4,7 +4,7 @@ public class View {
 
 	private int getInput(){
 		Scanner s = new Scanner(System.in);
-		System.out.println("hoi");
+		System.out.println("hoi, there");
 		return s.nextInt();
 		
 	}
